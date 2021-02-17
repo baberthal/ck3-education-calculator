68c35a5f78f031e326470e8a37d4fdbacf5678b6
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2021-02-17 14:58:22 -0700

    Main app files

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <b-container>
+      <h1 class="text-center">CK3 Education Calculator</h1>
+      <b-row>
+        <b-col>
+          <guardian-view :guardian="guardian" />
+        </b-col>
+        <b-col>
+          <ward-view :ward="ward" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { GuardianView, WardView } from "./components";
+import { Character } from "@/models";
 
 @Component({
   components: {
-    HelloWorld
+    GuardianView,
+    WardView
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  guardian: Character = new Character();
+  ward: Character = new Character();
+}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
+//===- main.ts - Main App File ---------------------------------------------===//
+//
+// Copyright (c) 2021 J. Morgan Lieberthal
+// Licensed under the MIT License
+//
+//===-----------------------------------------------------------------------===//
+
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
