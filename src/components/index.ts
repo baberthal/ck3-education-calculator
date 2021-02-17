849c11ec5ce1cccf21b890dc8bd883222bd014f5
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2021-02-17 14:58:12 -0700

    Add component export bucket

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+//===- components/index.ts - Export Bucket for Components ------------------===//
+//
+// Copyright (c) 2021 J. Morgan Lieberthal
+// Licensed under the MIT License
+//
+//===-----------------------------------------------------------------------===//
+
+export { AttributeInput } from "./attribute-input";
+export { GuardianAttributes } from "./guardian-attributes";
+export { GuardianView } from "./guardian-view";
+export { WardView } from "./ward-view";
