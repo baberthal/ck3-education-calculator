e5633bf978678c0037308356c48d732bcf4a2c2a
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2021-02-27 14:43:42 -0700

    Update @/components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,4 +9,5 @@ export { ChildhoodTraitSelect } from "./childhood-trait-select";
 export { EducationFocusSelect } from "./education-focus-select";
 export { GuardianView } from "./guardian-view";
 export { IntellectTraitSelect } from "./intellect-trait-select";
+export { ResultsView } from "./results-view";
 export { WardView } from "./ward-view";
